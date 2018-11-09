67ba17b18a595eef0d4dee1446a185beab81a4af
JOÃO HENRIQUE MAY SPENGLER <joao.spengler@univille.br>
2018-11-09 21:27:51 -0200

    First Commit

---
+++ src/main/java/Main/Task.java
@@ -0,0 +1,38 @@
+package Main;
+
+public class Task {
+
+	private Integer id;
+	private String description;
+	private boolean status;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [id=" + id + ", description=" + description + ", status=" + status + "]";
+	}
+
+}
